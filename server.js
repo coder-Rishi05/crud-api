5e8e479e636b61a5a72602302f782c14d0579750
coder-Rishi05 <rawatrishi1121@gmail.com>
2026-01-02 11:58:16 +0530

    setup backend and started server

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import { PORT } from "./src/constants/constant.js";
+
+dotenv.config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hi from the server");
+  console.log("Hi from the server");
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Serevr running at port : ${process.env.PORT || PORT} `);
+  console.log(
+    `visit this site at : http://localhost:${process.env.PORT || PORT}/`
+  );
+});
