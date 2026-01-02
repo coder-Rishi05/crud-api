195d2be8192cf090cf3d0b7aa3d86d1839e1f6cc
coder-Rishi05 <rawatrishi1121@gmail.com>
2026-01-02 12:53:50 +0530

    added modek and post route

--- server.js
+++ server.js
@@ -2,14 +2,47 @@ import express from "express";
 import dotenv from "dotenv";
 import { PORT } from "./src/constants/constant.js";
 import connectDb from "./src/db/db.js";
+import Product from "./src/models/product.model.js";
 
 await connectDb();
 dotenv.config();
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hi from the server");
   console.log("Hi from the server");
+});
+
+app.post("/api/products", async (req, res) => {
+  try {
+    const { name, quantity, price, image } = await req.body;
+
+    if (!name || !quantity || !price || !image) {
+      return res.status(404).json({ message: "All feilds are necessary" });
+    }
+
+    const createProduct = await Product.create({
+      name,
+      quantity,
+      price,
+      image,
+    });
+
+    return res.status(201).json({
+      message: "data received successfully",
+      data: {
+        name: createProduct.name,
+        quantity: createProduct.quantity,
+        price: createProduct.price,
+        image: createProduct.image,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: "server error" });
+    console.log(error);
+  }
 });
 
 app.listen(process.env.PORT || PORT, () => {
---
+++ src/models/product.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("product", productSchema);
+
+export default Product;
